3b553b92b37247af86c65e99bc6edfc48ff327dd
Eyuel763 <eyuelg1997@gmail.com>
2025-02-23 13:15:58 +0400

    modified django-models/LibraryProject/relationship_app/query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,25 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTING_MODULE", "django_models.settings")
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings") 
+
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-#Sample Data Creation
+# Sample Data Creation (for demonstration)
 author1 = Author.objects.create(name="Jane Doe")
-book1 = Book.objects.create(title="Python Bsics", author=author1)
+book1 = Book.objects.create(title="Python Basics", author=author1)
 book2 = Book.objects.create(title="Django Unleashed", author=author1)
 library1 = Library.objects.create(name="Central Library")
 library1.books.add(book1, book2)
 librarian1 = Librarian.objects.create(name="John Smith", library=library1)
 
+# Queries
 # Query all books by a specific author
 author_books = Book.objects.filter(author=author1)
-print("Books by Jane Doe: ")
+print("Books by Jane Doe:")
 for book in author_books:
     print(book.title)
 
