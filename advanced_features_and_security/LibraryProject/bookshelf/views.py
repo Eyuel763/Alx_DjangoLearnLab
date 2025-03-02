c91688174cdaebc97d90db44ba77482ab2525876
Eyuel763 <eyuelg1997@gmail.com>
2025-03-02 18:08:11 +0400

    modified

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponseForbidden
 from .models import Book
+from .forms import ExampleForm
 from .forms import BookForm
 from django.db.models import Q
 
